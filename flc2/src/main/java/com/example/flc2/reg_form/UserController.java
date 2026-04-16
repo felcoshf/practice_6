3ba583e881d30d15d1981e730da742001d945946
felcoshf <poprygunchik.chik@gmail.com>
2026-04-16 10:58:58 +0300

    Delete flc2 directory

--- flc2/src/main/java/com/example/flc2/reg_form/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.example.flc2.reg_form;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class UserController {
-
-}
--- flc2/src/main/java/com/example/flc2/reg_form/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.flc2.reg_form;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
